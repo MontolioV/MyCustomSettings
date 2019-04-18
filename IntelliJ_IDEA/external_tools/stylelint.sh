6019028bcfaf095ab8af961aa86d7e713c640d34
Vitaliy Podzigun <vitaliy.podzigun@toweya.net>
2019-04-18 17:54:43 +0300

    Stylelint external tool shell script for IntelliJ IDEA changed to use Node bin path from arguments.

--- IntelliJ_IDEA/external_tools/stylelint.sh
+++ IntelliJ_IDEA/external_tools/stylelint.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+# $1 - full path to lintered file
 filename=$(basename -- "$1")
 extension="${filename##*.}"
 
 if [[ "$extension" = "css" ||"$extension" = "scss" || "$extension" = "sass" ]]; then
-    PATH=$PATH:/home/user123/.nvm/versions/node/v10.15.2/bin
+    # $2 - path to node bins, example '/home/user123/.nvm/versions/node/v10.15.2/bin'
+    PATH=$PATH:$2
     stylelint --fix $1
 fi;
 
