3f402d2e242d626a93343033c8beec07a5c77cbd
Vitalii Podzigun <vpodzygun@jazzros.com>
2022-10-12 20:30:25 +0300

    rndpswd update.

--- code/rndpswd.js
+++ code/rndpswd.js
@@ -1,15 +1,30 @@
+// node code/rndpswd.js 20 ans
 let result = '';
-let iterations = process.argv[2]
+let passLength = process.argv[2]
   ? parseInt(process.argv[2])
   : 8 + ~~(Math.random() * 10);
-let asciiMin = 33;
-let asciiMax = 126;
+
+// a - use alphabet, n - use numbers, s - use special characters
+let mode = process.argv[3] ? process.argv[3] : 'ans'
+
+let numberChars = '0123456789';
+let specialChars = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+let alphabetChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+let chars = []
+if (mode.includes('a')){
+  chars = [...chars,...alphabetChars.split('')]
+}
+if (mode.includes('n')){
+  chars = [...chars,...numberChars.split('')]
+}
+if (mode.includes('s')){
+  chars = [...chars,...specialChars.split('')]
+}
 
 for (let i = 0; i < 20; i++) {
-  for (let i = 0; i < iterations; i++) {
-    result += String.fromCharCode(
-      asciiMin + ~~((asciiMax - asciiMin) * Math.random()),
-    );
+  for (let i = 0; i < passLength; i++) {
+    result += chars[~~(chars.length* Math.random())]
   }
 
   console.log(result);
--- nvm-switch.cmd
+++ nvm-switch.cmd
@@ -2,5 +2,5 @@ CALL nvm list
 
 IF [%1]==[] ECHO Value Missing
 IF [%1]==[14] CALL nvm use 14.17.4
-IF [%1]==[16] CALL nvm use 16.14.0
+IF [%1]==[16] CALL nvm use 16.16.0
 IF [%1]==[17] CALL nvm use 17.6.0
